3706cb4ab333bca91bd568d5426f4ef2e09d8717
xiaozhang <3077526065@qq.com>
2020-12-07 19:15:01 +0800

    hello3

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from users.models import User
 # Create your views here.
 from django.http import HttpResponse
 
@@ -7,4 +7,10 @@ from django.http import HttpResponse
 # /register/
 def register(request):
     """注册View视图函数"""
-    return render(request,'register.html')
+    if request.method == 'GET':
+        return render(request,'register.html')
+    else:
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = User.objects.create(username=username, password=password )
+        return HttpResponse("注册成功")
