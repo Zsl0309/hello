f64a1b9d1acfd97a4581301743f26fe999efc90f
xiaozhang <3077526065@qq.com>
2020-12-07 15:23:14 +0800

    hello

--- helloworld/views.py
+++ helloworld/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 def first_view_func(request):
-    return HttpResponse('<h6>hello world</h6>')
+    return HttpResponse('<h1>hello world</h1>')
